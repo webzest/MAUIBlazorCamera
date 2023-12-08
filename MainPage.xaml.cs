f1058b8413260858cedfd8cac9fa66c8ecb7ba27
Johnny Sandaire <sandaire@gmail.com>
2023-12-07 22:37:47 -0500

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components.WebView;
+using Microsoft.AspNetCore.Components.WebView.Maui;
+using Microsoft.Maui.Controls;
+
+namespace MAUIBlazorCamera;
+
+public partial class MainPage:ContentPage
+{   
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+}
