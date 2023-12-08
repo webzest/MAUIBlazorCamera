218c8de3606042f0a2657cd3c891107663daab2a
Johnny Sandaire <sandaire@gmail.com>
2023-12-08 11:58:36 -0500

    updated Pictures

--- App.xaml.cs
+++ App.xaml.cs
@@ -9,4 +9,17 @@
             MainPage = new MainPage();
         }
     }
+
+    public interface IAppInfoService
+    {
+        string GetApplicationName();
+    }
+
+    public class AppInfoService:IAppInfoService
+    {
+        public string GetApplicationName()
+        {
+            return "YourApp"; // Replace with your actual application name
+        }
+    }
 }
--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace MAUIBlazorCamera
 {
@@ -15,7 +16,7 @@ namespace MAUIBlazorCamera
                 });
 
             builder.Services.AddMauiBlazorWebView();
-
+            builder.Services.AddSingleton<IAppInfoService, AppInfoService>();
 
 #if DEBUG
             builder.Services.AddBlazorWebViewDeveloperTools();
